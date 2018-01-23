f55ed90ba3a3dc4b04d959b7c5eefa4e4310c7d2
VajkFreyasman <craig.linde.com>
2018-01-23 11:32:06 -0700

    added through datafeatures

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :portfolios
+  
+  get 'angular-items', to: 'portfolios#angular'
+
   get 'pages/home'
 
   get 'pages/about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+
+3.times do |topic|
+Topic.create!(
+  title: "Topic #{topic}")
+end
+
+p "3 topics created"
+
 10.times do |blog|
 Blog.create!(
   title: "My Blog Post #{blog}",
-  body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum nec #{blog}"
+  body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum nec #{blog}",
+  topic_id: Topic.last.id
   )
 end
 
@@ -16,16 +25,24 @@ end
 p "5 skills created"
 
 
-
-p "9 portfolio created"
-9.times do |item|
+8.times do |item|
   Portfolio.create!(
   title: "Portfolio #{item}",
+  subtitle: "Ruby on Rails",
   body: "quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum ",
   main_image: "http://via.placeholder.com/350x150.",
   thumb_image: "http://via.placeholder.com/140x100."
   )
-
 end
 
+1.times do |item|
+  Portfolio.create!(
+  title: "Portfolio #{item}",
+  subtitle: "Angular",
+  body: "quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur ",
+  main_image: "http://via.placeholder.com/350x150.",
+  thumb_image: "http://via.placeholder.com/140x100."
+  )
+end
+p "9 portfolio created"
 
