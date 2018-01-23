73d76bdfdda33959b013ac6d8a667ede514e2b61
VajkFreyasman <craig.linde.com>
2018-01-23 12:38:59 -0700

    added defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,11 +1,22 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
 
-  validates_presence_of :title, :body, :main_image
+
+  validates_presence_of :title, :body, :main_image, :thumb_image
 
  def self.angular
   where(subtitle: 'Angular')
 end
 
 #scope:ruby_on_rails, -> {where (subtitle: 'Ruby on Rails')}
+
+after_initialize :set_defaults
+
+def set_defaults
+self.main_image ||= Placeholder.image_generator(height: '350', width: '200')
+self.thumb_image ||= Placeholder.image_generator(height: '140', width: '100')
+
+end
+
 
 end
