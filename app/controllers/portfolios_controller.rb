7d39cc3442802b551212c66b16bb05f1fcbef492
VajkFreyasman <craig.linde.com>
2018-01-23 22:03:28 -0700

    added all master items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,4 @@
 class PortfoliosController < ApplicationController
-
   def index
     @portfolio_items = Portfolio.all
   end
@@ -20,16 +19,38 @@ end
 
 def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+=======
+    @portfolio_item = Portfolio.new
+  end
+
+  def show
+  @portfolio_item = Portfolio.find(params[:id])
+end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Post Removed.' }
+    end
+  end
+
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
 
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-        format.json { render :show, status: :created, location: @portfolio_item }
       else
         format.html { render :new }
-        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
 
@@ -53,3 +74,17 @@ def update
 
 
 end
+=======
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+end
+
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,4 +4,8 @@ class Blog < ApplicationRecord
 validates_presence_of :title, :body
 belongs_to :topic
 
+  enum status: {draft: 0, published: 1}
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
 end
