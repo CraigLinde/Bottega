89f0d2299dc40f6d19cdb9fd571ebc480ee644be
VajkFreyasman <craig.linde.com>
2018-02-07 15:38:40 -0700

    added styling for images and fixed bugs

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,9 +3,7 @@ class Portfolio < ApplicationRecord
     accepts_nested_attributes_for :technologies, 
                                   reject_if: lambda {|attrs| attrs['name'].blank?}
 
-  include Placeholder
-
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
 
 
   mount_uploader :thumb_image, PortfolioUploader
@@ -15,16 +13,9 @@ class Portfolio < ApplicationRecord
     where(subtitle: 'Angular')
   end
 
-  after_initialize :set_defaults
 
-def self.by_position
-  order("position ASC")
-end
-
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '350', width: '200')
-    self.thumb_image ||= Placeholder.image_generator(height: '140', width: '100')
+  def self.by_position
+    order("position ASC")
   end
 
 
