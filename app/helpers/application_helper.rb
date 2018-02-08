5937cfa0af52058c3222189eed6a8cb0d1bb8ced
VajkFreyasman <craig.linde.com>
2018-02-08 15:25:40 -0700

    refactored alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,4 +59,29 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+
+
+  def alerts
+      alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+      if alert
+        alert_generator alert
+      end
+  end
+
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Craig Linde's Portfolio", sticky: false)
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
